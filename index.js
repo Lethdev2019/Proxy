9f66d713a6cb3d4ecb0bac0677122c9bf9ad0b42
Dominic Parry <Lethiathan2019@outlook.com>
2020-07-28 20:06:18 +0000

    lots of stuff

--- index.js
+++ index.js
@@ -1,63 +1,126 @@
-// adcharirt
+// Dependencies
+const express = require('express')
+const path = require('path')
+const url = require('url')
+const proxy = require('unblocker')
+// var http = require('http');
+// var url = require('url');
+// var request = require('request');
 
-var http = require('http');
-var url = require('url');
-var request = require('request');
+const app = express()
 
-const path = require("path");
-const fs = require("fs");
+// Templating Configuration
+app.engine('html', require('ejs').renderFile)
+app.set('views', path.join(__dirname, '/templates'))
+app.set('view engine', 'html')
 
-var server = http.createServer(function(req, res) {  
-    var path = url.parse(req.url).pathname;  
-    switch (path) {  
-        case '/':  
-            fs.readFile('index.html', function(error, data) {  
-                if (error) {  
-                    res.writeHead(404);  
-                    res.write(error);  
-                    res.end();  
-                } else {  
-                    res.writeHead(200, {  
-                        'Content-Type': 'text/html'  
-                    });  
-                    res.write(data);  
-                    res.end();  
-                }  
-            });  
-            break;  
-        case '/proxy':  
-        	var queryData = url.parse(req.url, true).query;
-				if (queryData.url) {
-					request({
-						url: queryData.url
-					}).on('error', function(e) {
-						res.end(e);
-					}).pipe(res);
-				}
-				else {
-					res.end("no url found");
-				}
-            break;  
-        default:  
-            res.writeHead(404);  
-            res.write("opps this doesn't exist - 404");  
-            res.end();  
-            break;  
-    }  
-});  
-function onRequest(req, res) {
+// Static
+app.use(express.static('static'))
 
-    var queryData = url.parse(req.url, true).query;
-    if (queryData.url) {
-        request({
-            url: queryData.url
-        }).on('error', function(e) {
-            res.end(e);
-        }).pipe(res);
-    }
-    else {
-        res.end("no url found");
-    }
-}
-server.listen(8082);  
-//https://Proxy.adcharity.repl.co/?url=[your_url]
+// Requests
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// Routes
+app.get('/', (req, res) => {
+	res.render('index')
+	// res.sendFile(path.join(__dirname, 'pages/index.html'))
+    // let now = new Date();
+//   console.log("Page Request[/]: "+now);
+})
+
+//404 Error
+app.use((req, res) => {
+	res.status(404).render('404')
+})
+app.use(function(req, res, next) {
+    res.status(404);
+    res.sendFile(__dirname + '/templates/404.html');
+    let now = new Date();
+    console.log("404 Error: "+now);
+});
+
+
+app.listen(8080)
+''
+
+// var server = http.createServer(function(req, res) {  
+//     var path = url.parse(req.url).pathname;  
+//     switch (path) {  
+//         case '/':  
+//             fs.readFile('index.html', function(error, data) {  
+//                 if (error) {  
+//                     res.writeHead(404);  
+//                     res.write(error);  
+//                     res.end();  
+//                 } else {  
+//                     res.writeHead(200, {  
+//                         'Content-Type': 'text/html'  
+//                     });  
+//                     res.write(data);  
+//                     res.end('<head><title>Poly Mirror Service</title></head>');  
+//                 }  
+//             });  
+//             break;  
+//         case '/proxy':  
+// 			fs.readFile('partials/proxy.html', function(error, data) {  
+//                 if (error) {  
+//                     res.writeHead(404);  
+//                     res.write(error);  
+//                     res.end();  
+//                 } else {  
+//                     res.writeHead(200, {  
+//                         'Content-Type': 'text/html'  
+//                     });  
+//                     res.write(data);
+// 					res.end('<head><title>Poly Mirror Service</title></head>');  
+//                 }  
+//             });  
+// 			break; 
+// 		case '/process':  
+// 			try{
+// 			res.write('<head><title>Poly Mirror Service</title></head>')
+//         	var queryData = url.parse(req.url, true).query;
+// 				if (queryData.url) {
+// 					console.log('Webpage requested...')
+// 					request({
+// 						url: queryData.url
+// 					}).on('error', function(e) {
+// 						res.end(e);
+// 					}).pipe(res);
+// 				}
+// 				else {
+// 					res.end("<h1>404 error</h1><p>no url was detected</p><head><title>Poly Mirror Service</title></head>");
+// 				}
+// 			} catch(e) {
+// 				console.log('error');
+// 				res.write("<p>redirecting...</p> <script>window.location.replace('https://proxy.lethdev2019.repl.co/404.html');</script>");
+// 				res.end();
+// 			}
+//             break;   
+//         default:  
+//             res.writeHead(404);  
+//             res.write("<p>redirecting...</p> <script>window.location.replace('https://proxy.lethdev2019.repl.co/404.html');</script>");  
+//             res.end();  
+//             break;  
+//     }  
+// });  
+// function onRequest(req, res) {
+
+//     var queryData = url.parse(req.url, true).query;
+//     if (queryData.url) {
+//         request({
+//             url: queryData.url
+//         }).on('error', function(e) {
+//             res.end(e);
+//         }).pipe(res);
+//     }
+//     else {
+//         res.end("no url found");
+//     }
+// }
+// try{
+// 	server.listen(8082);
+// } catch {
+// 	server.listen(8082);
+// }
